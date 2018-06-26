c17b1a79129659c764d09ad2255a4acda2ac74f6
ryanchankh <ryanchankh@berkeley.edu>
2018-06-26 23:18:27 +0800

    perform test and added some results

--- style_transfer.py
+++ style_transfer.py
@@ -4,34 +4,34 @@ Last Updated: 17 June 2018
 Author: Ryan Chan
 """
 
-import tensorflow as tf
-import numpy as np 
-from PIL import Image
 import time
 from datetime import datetime
 
-import vgg19.vgg as vgg
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+
 import utils
+import vgg19.vgg as vgg
 
 
-
-class StyleTransfer(): 
+class StyleTransfer():
     """
-    Style Transfer Object. 
+    Style Transfer Object.
     """
-    
+
     def __init__(self, gen_img, img_shape, cont_layers, styl_layers, styl_layer_weights, styl_loss_alpha, cont_loss_beta, learning_rate):
         """
-        Initialize object. 
+        Initialize object.
 
-        Parameters: 
-            gen_img: the initialize image; image to be turned into 
+        Parameters:
+            gen_img: the initialize image; image to be turned into
             img_shape: the shape of the image
             cont_layers: the layers chosen to compute the content layers
             styl_layesr: the layers chosen to comupte the style layers
             styl_layer_weighs: the assicoiated weights for each style feature layer when computing style loss
             styl_loss_alpha: a scalar to adjust overall style loss
-            cont_loss_beta: a sclar to adjust overall content loss
+            cont_loss_beta: a scalar to adjust overall content loss
 
         """
         self.gen_img = gen_img
@@ -44,14 +44,14 @@ class StyleTransfer():
         self.cont_loss_beta = cont_loss_beta
         self.learning_rate = learning_rate
         self.graph = self.build_graph()
-        
+
     def build_graph(self):
         """
-        The graph for the whole style tranfser process. 
+        The graph for the whole style tranfser process.
         """
         with tf.Graph().as_default() as g:
             with tf.variable_scope("image_input") as scope:
-                self.image = tf.Variable(self.gen_img, trainable=True, dtype=tf.float32) 
+                self.image = tf.Variable(self.gen_img, trainable=True, dtype=tf.float32)
                 self.styl_img = tf.placeholder(tf.float32, shape=[1, None, None, None], name="style_image")
                 self.cont_img = tf.placeholder(tf.float32, shape=[1, None, None, None], name="content_image")
 
@@ -68,21 +68,21 @@ class StyleTransfer():
                 styl_model = self.vgg_net.build(self.styl_img)
                 self.styl_layer_ops = styl_model.layer_dict(self.styl_layers)
                 self.styl_activity_calc = {layer_name: self.styl_layer_ops[layer_name] for layer_name in self.styl_layers}
-        
+
                 self.styl_activity = {layer_name: tf.placeholder(tf.float32, shape=[1, None, None, None], name="styl_activity") for layer_name in self.styl_layers}
-                
+
             with tf.name_scope("cont_activity") as scope:
                 cont_model = self.vgg_net.build(self.cont_img)
                 self.cont_layer_ops = cont_model.layer_dict(self.cont_layers)
                 self.cont_activity_calc = {layer_name: self.cont_layer_ops[layer_name] for layer_name in self.cont_layers}
                 self.cont_activity = {layer_name: tf.placeholder(tf.float32, shape=[1, None, None, None]) for layer_name in self.cont_layers}
-                
+
             with tf.variable_scope("styl_loss") as scope:
                 self.styl_loss = self.styl_loss()
-                
+
             with tf.variable_scope("cont_loss") as scope:
                 self.cont_loss = self.cont_loss()
-                
+
             with tf.variable_scope("total_loss") as scope:
                 self.total_loss = self.styl_loss_alpha * self.styl_loss + self.cont_loss_beta * self.cont_loss
 
@@ -91,21 +91,21 @@ class StyleTransfer():
                 self.beta2 = tf.Variable(0.999)
                 self.episilon = tf.Variable(1)
                 self.train_ops = tf.train.GradientDescentOptimizer(self.learning_rate).minimize(self.total_loss,var_list=self.image)
-                
+
             with tf.name_scope("init") as scope:
                 self.init_op = tf.global_variables_initializer()
-            
+
         return g
 
     def grammian(self, features):
         """
         Compute grammain for the given layer.
-        
-        Parameters: 
+
+        Parameters:
             features: a tensor of shape (1, Height, Width, Channels)
 
         Return:
-            grammian of the matrix. 
+            grammian of the matrix.
         """
         features_shape = tf.shape(features)
         H = features_shape[1]
@@ -121,28 +121,26 @@ class StyleTransfer():
         Computes content loss.
         """
         layer_cont_losses = {}
-        
+
         # compute loss for each layer
-        for layer in self.cont_layers: 
+        for layer in self.cont_layers:
             layer_cont_losses[layer] = tf.losses.mean_squared_error(self.gen_cont_activity[layer],self.cont_activity[layer])
         cont_loss = tf.reduce_sum(list(layer_cont_losses.values()))
-        
+
         return cont_loss
-    
+
     def styl_loss(self):
         """
         Compute style loss.
         """
         layer_styl_losses = {}
-        
+
         # compute grammian for each layer
         for layer_name in self.styl_layers:
             channels = self.gen_styl_activity[layer_name].get_shape().as_list()[3]
             feature_map_size = self.img_shape[1] * self.img_shape[2]
             denom = 2 * (channels ** 2) * (feature_map_size ** 2)
             layer_styl_losses[layer_name] = (1 / denom) * tf.losses.mean_squared_error(self.grammian(self.gen_styl_activity[layer_name]), self.grammian(self.styl_activity[layer_name]))
-        
+
         total_styl_loss = tf.reduce_sum([self.styl_layer_weights[layer_name] * layer_styl_losses[layer_name] for layer_name in self.styl_layers])
         return total_styl_loss
-
-    
--- utils.py
+++ utils.py
@@ -1,11 +1,12 @@
 """
-Utilities. 
+Utilities.
 Date: 17 June 2018
 Author: Ryan Chan
 """
 
 import numpy as np
 from PIL import Image
+
 
 def optimal_dimension(cont_img_path, styl_img_path):
     """
@@ -17,27 +18,27 @@ def optimal_dimension(cont_img_path, styl_img_path):
 
 def load_image(path, shape=None):
     """
-    Turn image into numpy array. Also subtract all three channels by 
-    mean pixel values for better outcome. 
+    Turn image into numpy array. Also subtract all three channels by
+    mean pixel values for better outcome.
     """
 
     image = Image.open(path)
     if shape is not None:
         shape = (shape[1], shape[2])
         image = image.resize(shape)
-    img_array = np.asarray(image, dtype=np.float32) 
+    img_array = np.asarray(image, dtype=np.float32)
     img_array = np.expand_dims(img_array, axis=0)
     img_array[:, :, :, 0] -= 103.939
     img_array[:, :, :, 1] -= 116.779
     img_array[:, :, :, 2] -= 123.68
     img_array = img_array[:, :, :, ::-1]
-    
+
     print("Image loaded: ", path, "with dimension", shape)
     return img_array
 
 def save_image(path, img_array):
     """
-    Save image. 
+    Save image.
     """
 
     image = np.copy(img_array)
@@ -45,11 +46,11 @@ def save_image(path, img_array):
     save_img.save(path)
     print("Image saved as: ", path)
     return save_img
-    
+
 def show_img(array):
     """
-    Turn numpy array back into image. Also re-add all three channels by mean pixel 
-    values. 
+    Turn numpy array back into image. Also re-add all three channels by mean pixel
+    values.
     """
 
     img_array = np.copy(array[0])
